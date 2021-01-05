35b9176cb7a80f14f19245c7b567f4fedf9e30bb
KG17 <kevingray17@gmail.com>
2021-01-05 13:33:37 -0500

    set variables

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,26 @@
 function init() {
-    var CHART = d3.selectAll("bar.chart").node();
+   
+   //Set variable to establish placement of bar graph in html file
+
+    var CHART = d3.selectAll("#bar").node();
+
+    //Read in JSON file
+
+    d3.json("samples.json").then(importedData => {
+    var data = importedData;
+
+    //Pull test subjects IDs from data
+    testsubjects = data.names;
+    
+    //Get the naval samples from data
+    testsamples = data.samples;
+
+    //Fetch metadata from data
+    metadata = data.metadata;
+
+    
+
+
+
+    })
 }
