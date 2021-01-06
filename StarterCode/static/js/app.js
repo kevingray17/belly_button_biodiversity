bfaf23f050f30f22e2d69833a25e1a3f65140c90
KG17 <kevingray17@gmail.com>
2021-01-06 09:46:07 -0500

    final code cleanup 9:46 AM

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -62,7 +62,7 @@ function init() {
   
     // // Apply the group bar mode to the layout
     var layout = {
-      title: "Top 10 OTUs found in the individual subject"
+      title: "Top 10 OTUs Found in Chosen Individuals"
     };
   
     // Render the plot to the div tag with id "bar"
@@ -85,7 +85,7 @@ function init() {
     var data = [trace1];
     
     var layout = {
-      title: 'All samples taken for the individual subject',
+      title: 'All Samples Collected from the Chosen Subject',
       showlegend: false
     };
       Plotly.newPlot('bubble', data, layout);
@@ -174,7 +174,7 @@ function init() {
       var data = [trace1];
       
       var layout = {
-        title: 'All samples taken for the individual subject',
+        title: 'All Samples Collected from the Chosen Subject',
         showlegend: false
       };
       
