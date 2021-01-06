975c32b572430ba845824a3530e4e11ad3b117b6
KG17 <kevingray17@gmail.com>
2021-01-06 10:06:49 -0500

    10:05

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,41 +1,47 @@
-    // Bar Chart Code
+// ***********************************************************************
+// This program is included as a script in index.html and provides the necessary 
+// functionality to load and plot data from a JSON file samples.json that includes
+// research data on bacteria growth in human navels. 
+// Program developed and tested by : Paul Hardy
+// Program developed and tested on : 10-07-2020
+// ************************************************************************
 
 function init() {
   
-    // Create variable setting bar chart's location in html file
+    // Variable to select the location of the bar chart from index.html
     var CHART = d3.selectAll("#bar").node();
   
-    // Fetch data in the json
+    // Use D3 fetch to read the JSON file into importedData as the argument.
       d3.json("samples.json").then(importedData => {
       var data = importedData;
   
-    // Grab the "testsubject ids" from the data 
-      testsubjects = data.names;
+    // Get just the "testsubject ids" from the data set
+      justtestsubjects = data.names;
   
-    // Grab the "samples" from the data set
-      samples = data.samples;
+    // Get just the "samples" from the data set
+      justsamples = data.samples;
   
-    // Grab the "metadata" from the data set
-      metadata = data.metadata;
+    // Get just the "metadata" from the data set
+      justmetadata = data.metadata;
   
     // Pre-Populate data into the Test Subject ID select dropdown list
     // Select the d3 input element for the dropdown
       const subjectselect = d3.select("#selDataset");
   
-    // Create the Test Subject ID dropdown
-      testsubjects.forEach(namevalue =>{
+    // Build the Test Subject ID drop down list
+      justtestsubjects.forEach(namevalue =>{
         var option = subjectselect.append("option");
         option.text(namevalue);
         option.attr("value",namevalue);
       });
   
     // Build arrays for the primary axes and labels for the bar plot.
+    // [0] is used because on init() the first subject ID is 0 
+      let sampvalues = justsamples[0].sample_values;
+      let otuids = justsamples[0].otu_ids;
+      let otulabels = justsamples[0].otu_labels;
     
-      let sampvalues = samples[0].sample_values;
-      let otuids = samples[0].otu_ids;
-      let otulabels = samples[0].otu_labels;
-    
-    // Slice the first 10 objects for plotting on bar chart
+    // Slice the first 10 objects for plotting - bar chart requirement
       sampvalues = sampvalues.slice(0, 10);
       otuids = otuids.slice(0, 10);
       otulabels = otulabels.slice(0,10);
@@ -62,22 +68,23 @@ function init() {
   
     // // Apply the group bar mode to the layout
     var layout = {
-      title: "Top 10 OTUs Found in Chosen Individuals"
+      title: "Top 10 OTUs found in the individual subject"
     };
   
     // Render the plot to the div tag with id "bar"
     Plotly.newPlot(CHART, chartData, layout);
     
-    // Bubble Chart Code
-
+    //***********************************
+    // Plot the Bubble chart  
+    //***********************************
     var trace1 = {
-      x: samples[0].otu_ids,
-      y: samples[0].sample_values,
-      text: samples[0].otu_labels,
+      x: justsamples[0].otu_ids,
+      y: justsamples[0].sample_values,
+      text: justsamples[0].otu_labels,
       mode: 'markers',
       marker: {
-        size: samples[0].sample_values,
-        color: samples[0].otu_ids,
+        size: justsamples[0].sample_values,
+        color: justsamples[0].otu_ids,
         colorscale: [[0,'rgb(0,0,255)'],[1,'rgb(255,0,0)']]
       }
     };
@@ -85,31 +92,35 @@ function init() {
     var data = [trace1];
     
     var layout = {
-      title: 'All Samples Collected from the Chosen Subject',
+      title: 'All samples taken for the individual subject',
       showlegend: false
     };
       Plotly.newPlot('bubble', data, layout);
   
-
-    // Create the Demographic Data panel
+    //*************************************
+    // Populate the Demographic Data panel
+    //*************************************
+      d3.select("#sample-metadata").append("p").text('ID: '+ justmetadata[0].id);
+      d3.select("#sample-metadata").append("p").text('Ethnicity: '+ justmetadata[0].ethnicity);
+      d3.select("#sample-metadata").append("p").text('Gender: '+ justmetadata[0].gender);
+      d3.select("#sample-metadata").append("p").text('Age: '+ justmetadata[0].age);
+      d3.select("#sample-metadata").append("p").text('Location: '+ justmetadata[0].location);
+      d3.select("#sample-metadata").append("p").text('Bbtype: '+ justmetadata[0].bbtype);
+      d3.select("#sample-metadata").append("p").text('Wfreq: '+ justmetadata[0].wfreq);
     
-      d3.select("#sample-metadata").append("p").text('ID: '+ metadata[0].id);
-      d3.select("#sample-metadata").append("p").text('Ethnicity: '+ metadata[0].ethnicity);
-      d3.select("#sample-metadata").append("p").text('Gender: '+ metadata[0].gender);
-      d3.select("#sample-metadata").append("p").text('Age: '+ metadata[0].age);
-      d3.select("#sample-metadata").append("p").text('Location: '+ metadata[0].location);
-      d3.select("#sample-metadata").append("p").text('Bbtype: '+ metadata[0].bbtype);
-      d3.select("#sample-metadata").append("p").text('Wfreq: '+ metadata[0].wfreq);
-    
-      
+     //***********************************************
      // Plot the Belly button washing Gauge chart 
-     
+     //***********************************************
        initGaugeChart();
     });
   };  
   // End of init() function
-  // Call updatePlotly() when a change takes place to the DOM
+
   
+  
+  //***********************************************************
+  // Call updatePlotly() when a change takes place to the DOM
+  //***********************************************************
   
   d3.selectAll("body").on("change", updatePlotly);
   
@@ -123,14 +134,15 @@ function init() {
     // Assign the value of the dropdown menu option to a variable
     var dataset = subjectselect.node().value;
       
-    // Find the index of the subject in the testsubjects array
-
-      let subjectindex = testsubjects.indexOf(dataset);
+    // Find the index of the subject in the justtestsubjects array
+    // This is necessary to find all of the dependant data for the 
+    // selected subject
+      let subjectindex = justtestsubjects.indexOf(dataset);
     
     // Build arrays for the primary axes and labels for the bar plot.
-      let sampvalues = samples[subjectindex].sample_values;
-      let otuids = samples[subjectindex].otu_ids;
-      let otulabels = samples[subjectindex].otu_labels;
+      let sampvalues = justsamples[subjectindex].sample_values;
+      let otuids = justsamples[subjectindex].otu_ids;
+      let otulabels = justsamples[subjectindex].otu_labels;
   
     // Slice the first 10 objects for plotting - bar chart requirement
       sampvalues = sampvalues.slice(0, 10);
@@ -150,31 +162,32 @@ function init() {
       y = otuidslist;
       text = otulabels;
   
-   
+    //***********************************
     // Re-style the Bar chart  
-    
+    //***********************************
       Plotly.restyle(CHART, "x", [x]);
       Plotly.restyle(CHART, "y", [y]);
       Plotly.restyle(CHART, "text", [text]);
   
-    
+    //***********************************
     // Re-Plot the Bubble chart  
+    //***********************************
       var trace1 = {
-        x: samples[subjectindex].otu_ids,
-        y: samples[subjectindex].sample_values,
-        text: samples[subjectindex].otu_labels,
+        x: justsamples[subjectindex].otu_ids,
+        y: justsamples[subjectindex].sample_values,
+        text: justsamples[subjectindex].otu_labels,
         mode: 'markers',
         marker: {
-          size: samples[subjectindex].sample_values,
-          color: samples[subjectindex].otu_ids,
-          colorscale: [[0,'rgb(0,0,2)'],[1,'rgb(255,0,0)']]
+          size: justsamples[subjectindex].sample_values,
+          color: justsamples[subjectindex].otu_ids,
+          colorscale: [[0,'rgb(0,0,255)'],[1,'rgb(255,0,0)']]
         }
       };
       
       var data = [trace1];
       
       var layout = {
-        title: 'All Samples Collected from the Chosen Subject',
+        title: 'All samples taken for the individual subject',
         showlegend: false
       };
       
@@ -184,18 +197,18 @@ function init() {
   
       d3.select("#sample-metadata").selectAll('p').remove();
   
-      d3.select("#sample-metadata").append("p").text('ID: '+ metadata[subjectindex].id);
-      d3.select("#sample-metadata").append("p").text('Ethnicity: '+ metadata[subjectindex].ethnicity);
-      d3.select("#sample-metadata").append("p").text('Gender: '+ metadata[subjectindex].gender);
-      d3.select("#sample-metadata").append("p").text('Age: '+ metadata[subjectindex].age);
-      d3.select("#sample-metadata").append("p").text('Location: '+ metadata[subjectindex].location);
-      d3.select("#sample-metadata").append("p").text('Bbtype: '+ metadata[subjectindex].bbtype);
-      d3.select("#sample-metadata").append("p").text('Wfreq: '+ metadata[subjectindex].wfreq);
+      d3.select("#sample-metadata").append("p").text('ID: '+ justmetadata[subjectindex].id);
+      d3.select("#sample-metadata").append("p").text('Ethnicity: '+ justmetadata[subjectindex].ethnicity);
+      d3.select("#sample-metadata").append("p").text('Gender: '+ justmetadata[subjectindex].gender);
+      d3.select("#sample-metadata").append("p").text('Age: '+ justmetadata[subjectindex].age);
+      d3.select("#sample-metadata").append("p").text('Location: '+ justmetadata[subjectindex].location);
+      d3.select("#sample-metadata").append("p").text('Bbtype: '+ justmetadata[subjectindex].bbtype);
+      d3.select("#sample-metadata").append("p").text('Wfreq: '+ justmetadata[subjectindex].wfreq);
   
-     
+    
      // Call function to restyle Belly button washing Gauge chart restyle
+     
 
-  
       restyleGaugeChart(subjectindex);
   }
   // End of updatePlotly()
